6ab012d87d90951d482edc0bcc6e4ad03ef9786c
楊明哲 <t109590009@ntut.edu.tw>
2022-01-12 11:35:51 +0800

    init commit

---
+++ fin.js
@@ -0,0 +1,69 @@
+// function show_hide() {
+//     var login = document.getElementById("container1");
+//     var signup = document.getElementById("container2");
+  
+//     if (login.style.display === "none") {
+//         login.style.display = "block";  //login出現
+//         document.getElementById("username").value="";
+//         document.getElementById("password").value="";
+//         signup.style.display = "block";  //signup消失
+//     } 
+//     else {
+//         login.style.display = "none";   //login消失
+//         signup.style.display = "block"; //signup出現
+//         signup.style.visibility="visible";
+     
+//         document.getElementById("fullname").value="";
+//         document.getElementById("username2").value="";
+//         document.getElementById("password2").value="";
+//         document.getElementById("comfirm_password").value="";
+//     }
+// }
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDOE5cr0k9818IXDnkIeYUMuf-_RbwrUxQ",
+    authDomain: "finally-59ead.firebaseapp.com",
+    projectId: "finally-59ead",
+    storageBucket: "finally-59ead.appspot.com",
+    messagingSenderId: "831861025589",
+    appId: "1:831861025589:web:6917e25ed2833451b4ba7c"
+  };
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+// db 
+//     .collection("user_manage")
+//     .get()
+//     .then(doclist => {
+//         doclist.forEach(element => {
+//             const user = element.data();
+//             const col = `<li class="list-group-item">
+//                             <span class="box bg-${user.color}"></span>
+//                             ${user.title}
+//                         </li> `
+//             $("#listGroup").append(col);
+//         });
+//     })
+
+const creatTodoButton = document.querySelector('button');
+creatTodoButton.addEventListener('click', addTodo);
+async function addTodo(event) {
+    event.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    await firebase.auth().signInWithEmailAndPassword(username, password)
+            .then((userCredential) => {
+            // Signed in
+            var user = userCredential.user;
+            // ...
+        })
+            .catch((error) => {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+        });
+    console.log("test");
+    alert("sign in finish.");
+    window.location.replace("index.html");
+}
---
+++ re.js
@@ -0,0 +1,58 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDOE5cr0k9818IXDnkIeYUMuf-_RbwrUxQ",
+    authDomain: "finally-59ead.firebaseapp.com",
+    projectId: "finally-59ead",
+    storageBucket: "finally-59ead.appspot.com",
+    messagingSenderId: "831861025589",
+    appId: "1:831861025589:web:6917e25ed2833451b4ba7c"
+  };
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+// import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.2/firebase-auth.js";
+// db 
+//     .collection("user_manage")
+//     .get()
+//     .then(doclist => {
+//         // doclist.forEach(element => {
+//         //     const user = element.data();
+//         //     const col = `<li class="list-group-item">
+//         //                     <span class="box bg-${user.color}"></span>
+//         //                     ${user.title}
+//         //                 </li> `
+//         //     $("#listGroup").append(col);
+//         // });
+//     })
+const creatTodoButton = document.querySelector('button');
+creatTodoButton.addEventListener('click', addTodo);
+async function addTodo(event) {
+    event.preventDefault();
+    const fullname = document.getElementById("fullname").value;
+    const username2 = document.getElementById("username2").value;
+    const password2 = document.getElementById("password2").value;
+    const comfirm_password = document.getElementById("comfirm_password").value;
+    await firebase.auth().createUserWithEmailAndPassword(username2, password2)
+        .then((userCredential) => {
+            // Signed in
+            var user = userCredential.user;
+            // ...
+            console.log("i want to sign up account.");
+        })
+        .catch((error) => {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            alert("sign up error.");
+            window.location.reload();
+        });
+    await db.collection('user_manage').add({
+        fullname,
+        username2,
+        password2,
+        comfirm_password
+    })
+    console.log("test");
+    alert("sign up finish.");
+    window.location.reload();
+
+}
